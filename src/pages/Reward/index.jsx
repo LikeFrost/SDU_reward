9ad4c939244b9a5c5e11527315da9320b111ce9a
LikeFrost <1342230493@qq.com>
2022-03-12 11:33:01 +0800

    修改reward

--- src/pages/Reward/index.jsx
+++ src/pages/Reward/index.jsx
@@ -4,7 +4,7 @@ import { Loading } from '@alifd/next';
 import store from '@/store';
 
 function Reward() {
-  const [switchTag, setTag] = useState('添加记录');
+  const [switchTag, setTag] = useState('添加奖励');
   const tabConfig = [
     {
       tag: '奖励总览',
@@ -32,7 +32,30 @@ function Reward() {
     },
     {
       tag: switchTag,
-      type: switchTag,
+      type: 'change_tag',
+      id: 'change_tag',
+    },
+  ];
+  const optionConfig = [
+    {
+      title: '请选择奖励所属类别',
+      options: ['未选择', '研究创新', '创业实践', '社会服务', '美育素养', '体育素养'],
+    },
+    {
+      title: '请选择赋分项目',
+      options: ['竞赛', '论文'],
+    },
+    {
+      title: '请输入奖项名称',
+      options: [],
+    },
+    {
+      title: '请选择奖项级别',
+      options: ['国家级', '省级', '市级', '校级', '书院级', '院级', '班级'],
+    },
+    {
+      title: '请选择奖项等次',
+      options: ['特等奖', '一等奖', '二等奖', '三等奖', '优胜奖', '参与奖'],
     },
   ];
   const [currentType, setCurrentType] = useState('reward_total');
@@ -50,6 +73,7 @@ function Reward() {
   const loadData = (type, tag) => {
     setCurrentType(type);
     setLoading(true);
+    setTag('添加奖励');
     if (tag === '奖励总览') {
       dispatchers_reward.getAllReward().then(() => {
         setTimeout(() => {
@@ -64,6 +88,11 @@ function Reward() {
       });
     }
   };
+  const getDetail = (id) => {
+    const changeTag = document.getElementById('change_tag');
+    changeTag.click();
+    setTag('奖励详情');
+  };
   return (
     <div className={styles.box}>
 
@@ -71,7 +100,7 @@ function Reward() {
         {
           tabConfig.map((item, index) => {
             return (
-              <div className={item.type === currentType ? styles.tab_sel : styles.tab} key={index} onClick={() => { loadData(item.type, item.tag); }}>{item.tag}</div>
+              <div id={item.id} className={item.type === currentType ? styles.tab_sel : styles.tab} key={index} onClick={() => { loadData(item.type, item.tag); }}>{item.tag}</div>
             );
           })
         }
@@ -81,7 +110,7 @@ function Reward() {
         visible={loading}
       >
         <div className={styles.content}>
-          {switchTag !== currentType &&
+          {currentType !== 'change_tag' &&
             <table className={styles.table}>
               <thead>
                 <tr className={styles.tr}>
@@ -98,7 +127,7 @@ function Reward() {
               <tbody>
                 {reward.map((item) => {
                   return (
-                    <tr key={item.Id}>
+                    <tr key={item.Id} className={styles.item}>
                       <td>{item.Tag}</td>
                       <td>{item.Type}</td>
                       <td>{item.Name}</td>
@@ -106,12 +135,34 @@ function Reward() {
                       <td>{item.Prize}</td>
                       <td>{item.Time}</td>
                       <td>{item.Score}</td>
-                      <td>查看</td>
+                      <td onClick={() => getDetail(item.Id)} style={{ cursor: 'pointer' }}>查看</td>
                     </tr>
                   );
                 })}
               </tbody>
             </table>
+          }
+          {currentType === 'change_tag' &&
+            <div className={styles.details}>
+              {
+                optionConfig.map((item) => {
+                  return (
+                    <div className={styles.detail}>
+                      <div className={styles.detail_tab}>{item.title}</div>
+                      <select className={styles.detail_option}>
+                        {
+                          item.options.map((i) => {
+                            return (
+                              <option value={i}>{i}</option>
+                            );
+                          })
+                        }
+                      </select>
+                    </div>
+                  );
+                })
+              }
+            </div>
           }
         </div>
       </Loading>
